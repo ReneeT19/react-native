a1a4d5cf7e22ca2db27d743fc9aec44f7cb6283f
Renee Thomsen <rcthomsen19@gmail.com>
2020-10-06 17:36:37 -0400

    slider and stepper

--- UdaciFitness/components/AddEntry.js
+++ UdaciFitness/components/AddEntry.js
@@ -1,8 +1,20 @@
-import React, { Component } from "react";
-import { View } from "react-native";
-import { getMetricMetaInfo } from "../utils/helpers";
+import React, { Component } from 'react'
+import { View, TouchableOpacity, Text } from 'react-native'
+import { getMetricMetaInfo, timeToString } from '../utils/helpers'
 import UdaciSlider from './UdaciSlider'
 import UdaciSteppers from './UdaciSteppers'
+import DateHeader from './DateHeader'
+import { Ionicons } from "@expo/vector-icons"
+import TextButton from "./TextButton"
+
+function SubmitBtn ({ onPress }) {
+  return (
+    <TouchableOpacity>
+      <Text onPress={onPress}>
+      SUBMIT</Text>
+    </TouchableOpacity>
+  )
+}
 
 export default class AddEntry extends Component {
   state = {
@@ -39,11 +51,40 @@ export default class AddEntry extends Component {
       [metric]: value
     }))
   }
+  submit = () => {
+    const key = timeToString()
+    const entry = this.state
+
+    // Update Redux
+
+    this.setState(() => ({ run: 0, bike: 0, swim: 0, sleep: 0, eat: 0 }))
+
+    // Navigate to home
+
+    // Save to "DB"
+
+    // Clear local notification
+  };
+  reset = () => {
+    const key = timeToString();
+  }
   render() {
     const metaInfo = getMetricMetaInfo()
 
+    if (this.props.alreadyLogged) {
+      return (
+        <View>
+          <Ionicons name={"md-happy"} size={100} />
+          <Text>You already logged your information for today.</Text>
+          <TextButton onPress={this.reset}>Reset</TextButton>
+        </View>
+      );
+    }
+
     return (
       <View>
+        <DateHeader date={(new Date()).toLocaleDateString()}/>
+        {/* testing purpose <Text>{JSON.stringify(this.state)}</Text>   */}
         {Object.keys(metaInfo).map((key) => {
           const { getIcon, type, ...rest } = metaInfo[key]
           const value = this.state[key]
@@ -66,6 +107,7 @@ export default class AddEntry extends Component {
             </View>
           )
         })}
+        <SubmitBtn onPress={this.submit} />
       </View>
     )
   }
--- UdaciFitness/components/UdaciSteppers.js
+++ UdaciFitness/components/UdaciSteppers.js
@@ -1,10 +1,24 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { View, TouchableOpacity, Text } from 'react-native'
+import { FontAwesome, Entypo } from '@expo/vector-icons'
+import { AntDesign } from '@expo/vector-icons'
+// import { gray } from './colors'
 
-export default function UdaciSteppers () {
+export default function UdaciSteppers ({ max, unit, step, value, onIncrement, onDecrement }) {
   return (
     <View>
-      <Text>UdaciSteppers</Text>
+      <View>
+        <TouchableOpacity onPress={onDecrement}>
+        <AntDesign name="minuscircle" size={24} color= "gray" />
+        </TouchableOpacity>
+        <TouchableOpacity onPress={onIncrement}>
+        <AntDesign name="pluscircle" size={24} color= "gray" />
+        </TouchableOpacity>
+      </View>
+      <View>
+        <Text>{value}</Text>
+        <Text>{unit}</Text>
+      </View>
     </View>
   )
 }
