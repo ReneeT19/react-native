18b56ab136ff3fb3b2c3124c490118b25705cf30
Renee Thomsen <rcthomsen19@gmail.com>
2020-10-19 15:20:15 -0400

    added denied status

--- UdaciFitness/components/Live.js
+++ UdaciFitness/components/Live.js
@@ -6,7 +6,7 @@ import { purple, white } from '../utils/colors'
 export default class Live extends Component{
     state = {
         coords: null,
-        status: 'undetermined',
+        status: 'denied',
         direction: '',
     }
     askPermission = () => {
@@ -20,8 +20,11 @@ export default class Live extends Component{
         }
         if (status === 'denied'){
             return(
-                <View>
-                    <Text>Denied</Text>
+            <View style={styles.center}>
+          <Foundation name='alert' size={50} />
+          <Text>
+            You denied your location. You can fix this by visiting your settings and enabling location services for this app.
+          </Text>
                 </View>
             )
         }
@@ -65,7 +68,7 @@ const styles = StyleSheet.create({
     },
     button: {
       padding: 10,
-      backgroundColor: "plum",
+      backgroundColor: "cadetblue",
       alignSelf: 'center',
       borderRadius: 5,
       margin: 20,
