013341c52c1e6486763424c1588cc51d52e38c4f
Renee Thomsen <rcthomsen19@gmail.com>
2020-10-22 22:43:10 -0400

    updates

--- UdaciFitness/components/Live.js
+++ UdaciFitness/components/Live.js
@@ -171,7 +171,7 @@ export default class Live extends Component {
       flex: 1,
       paddingTop: 15,
       paddingBottom: 15,
-      backgroundColor: 'lightblue',
+      backgroundColor: 'lightpurple',
       marginTop: 20,
       marginBottom: 20,
       marginLeft: 10,
